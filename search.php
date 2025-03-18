1ca34f6b5daaa41aa1e5b28d910f8d063ff2cbe3
Weiqiang Chen <weiqiangchen19@gmail.com>
2025-03-18 12:02:23 -0400

    fonctionnement de l'engin de recherche

--- search.php
+++ search.php
@@ -4,18 +4,27 @@
  */
 ?>
 
-<?php get_header() ?>
-    <h1>search.php</h1>
-    <section class="populaire">
-        <div class="global">
-            <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+<?php get_header(); ?>
+
+<h1>Résultats de recherche pour : <?php echo get_search_query(); ?></h1>
+
+<section class="populaire">
+    <div class="global">
+        <?php if (have_posts()) : ?>
+            <?php while (have_posts()) : the_post(); ?>
                 <article class="populaire__article">
-                    <h2 class="populaire__titre"><?php the_title(); ?></h2>
-                    <div class="pouplaire__contenu"><?php echo wp_trim_words(get_the_excerpt(), 50, "..."); ?></div>
+                    <h2 class="populaire__titre">
+                        <a href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
+                    </h2>
+                    <div class="populaire__contenu">
+                        <?php echo wp_trim_words(get_the_excerpt(), 50, "..."); ?>
+                    </div>
                 </article>
-            <?php endwhile; endif; ?>
-        </div>
-    </section>
-   <?php get_footer(); ?>
-</body>
-</html>
+            <?php endwhile; ?>
+        <?php else : ?>
+            <p>Aucun résultat trouvé. Essayez une autre recherche.</p>
+        <?php endif; ?>
+    </div>
+</section>
+
+<?php get_footer(); ?>
