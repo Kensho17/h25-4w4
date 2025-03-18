eb066794a9e5a368ee68c1fc7b16053a99175b4c
Weiqiang <weiqiangchen19@gmail.com>
2025-03-18 16:53:24 -0400

    correction de l'ordre des elements sur la carte

--- gabarit/carte.php
+++ gabarit/carte.php
@@ -5,8 +5,6 @@ gabarit permettant d'afficher une carte
 
 ?>
 <article class="carte carte--grande">
-                     <figure class="carte__image">
-                    </figure>
                     <div class="carte__contenu">
                         <?php
                         if(has_post_thumbnail()){
@@ -14,11 +12,11 @@ gabarit permettant d'afficher une carte
                             the_post_thumbnail('thumbnail'); }
                         ?>
                     <h2 class="carte__titre"><?php the_title(); ?> </h2>
-                    <p class="carte__description"><?php echo wp_trim_words(get_the_excerpt(), 20, "..."); ?></p>
-                    <a class="carte__bouton carte__bouton--actif" href="<?php the_permalink() ?>">Suite</a>
                     <?php the_category() ?>
                     <p>Température minimum <?php  echo the_field('temperature_minimum'); ?>&#8451; </p>
                     <p>Température maximum <?php echo the_field('temperature_maximum'); ?>&#8451; </p>
                     <p>Température moyenne <?php echo the_field('temperature_moyenne'); ?>&#8451;</p>
+                    <p class="carte__description"><?php echo wp_trim_words(get_the_excerpt(), 20, "..."); ?></p>
+                    <a class="carte__bouton carte__bouton--actif" href="<?php the_permalink() ?>">Suite</a>
                 </div>
 </article>
