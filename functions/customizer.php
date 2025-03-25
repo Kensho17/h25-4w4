3b845d89871bafa92179660f712dbe69a0f40ed7
Weiqiang <weiqiangchen19@gmail.com>
2025-03-25 17:32:34 -0400

    correction du readme

--- functions/customizer.php
+++ functions/customizer.php
@@ -4,32 +4,32 @@
  *
  * @param WP_Customize_Manager $wp_customize L'objet du personnalisateur WordPress.
  */
-function theme_30w_customize_register($wp_customize) {
+function theme_31w_customize_register($wp_customize) {
 
     // ====== Section Hero ======
     $wp_customize->add_section('hero_section', array(
-        'title'    => __('Hero Section', 'theme_30w'),
-        'priority' => 30,
+        'title'    => __('Hero Section', 'theme_31w'),
+        'priority' => 31,
     ));
 
     // Titre principal
     $wp_customize->add_setting('hero_auteur', array(
-        'default'           => __('Weiqiang Chen', 'theme_30w'),
+        'default'           => __('Weiqiang Chen', 'theme_31w'),
         'sanitize_callback' => 'sanitize_text_field',
     ));
     $wp_customize->add_control('hero_auteur', array(
-        'label'   => __('Auteur', 'theme_30w'),
+        'label'   => __('Auteur', 'theme_31w'),
         'section' => 'hero_section',
         'type'    => 'text',
     ));
 
     // Courriel
     $wp_customize->add_setting('hero_courriel', array(
-        'default'           => __('Weiqiang Chen', 'theme_30w'),
+        'default'           => __('Weiqiang Chen', 'theme_31w'),
         'sanitize_callback' => 'sanitize_text_field',
     ));
     $wp_customize->add_control('hero_courriel', array(
-        'label'   => __('Courriel', 'theme_30w'),
+        'label'   => __('Courriel', 'theme_31w'),
         'section' => 'hero_section',
         'type'    => 'text',
     ));
@@ -40,45 +40,45 @@ function theme_30w_customize_register($wp_customize) {
         'sanitize_callback' => 'esc_url_raw',
     ));
     $wp_customize->add_control(new WP_Customize_Image_Control($wp_customize, 'hero_background', array(
-        'label'   => __('Image en background', 'theme_30w'),
+        'label'   => __('Image en background', 'theme_31w'),
         'section' => 'hero_section',
     )));
 
     // ====== Section Footer ======
     $wp_customize->add_section('footer_section', array(
-        'title'    => __('Section pied de page', 'theme_30w'),
+        'title'    => __('Section pied de page', 'theme_31w'),
         'priority' => 30,
     ));
 
     // Mission
     $wp_customize->add_setting('footer_mission', array(
-        'default'           => __('Mission du club de voyage', 'theme_30w'),
+        'default'           => __('Mission du club de voyage', 'theme_31w'),
         'sanitize_callback' => 'sanitize_text_field',
     ));
     $wp_customize->add_control('footer_mission', array(
-        'label'   => __('Mission', 'theme_30w'),
+        'label'   => __('Mission', 'theme_31w'),
         'section' => 'footer_section',
         'type'    => 'text',
     ));
 
     // Adresse
     $wp_customize->add_setting('footer_adresse', array(
-        'default'           => __('Adresse', 'theme_30w'),
+        'default'           => __('Adresse', 'theme_31w'),
         'sanitize_callback' => 'sanitize_text_field',
     ));
     $wp_customize->add_control('footer_adresse', array(
-        'label'   => __('Adresse', 'theme_30w'),
+        'label'   => __('Adresse', 'theme_31w'),
         'section' => 'footer_section',
         'type'    => 'text',
     ));
 
     // Téléphone
     $wp_customize->add_setting('footer_telephone', array(
-        'default'           => __('Telephone', 'theme_30w'),
+        'default'           => __('Telephone', 'theme_31w'),
         'sanitize_callback' => 'sanitize_text_field',
     ));
     $wp_customize->add_control('footer_telephone', array(
-        'label'   => __('Telephone', 'theme_30w'),
+        'label'   => __('Telephone', 'theme_31w'),
         'section' => 'footer_section',
         'type'    => 'text',
     ));
@@ -89,7 +89,7 @@ function theme_30w_customize_register($wp_customize) {
         'sanitize_callback' => 'esc_url_raw',
     ));
     $wp_customize->add_control(new WP_Customize_Color_Control($wp_customize, 'hero_icone', array(
-        'label'   => __('Couleur du texte', 'theme_30w'),
+        'label'   => __('Couleur du texte', 'theme_31w'),
         'section' => 'hero_section',
     )));
 
@@ -99,7 +99,7 @@ function theme_30w_customize_register($wp_customize) {
         'sanitize_callback' => 'esc_url_raw',
     ));
     $wp_customize->add_control(new WP_Customize_Color_Control($wp_customize, 'hero_texte', array(
-        'label'   => __('Couleur du texte', 'theme_30w'),
+        'label'   => __('Couleur du texte', 'theme_31w'),
         'section' => 'hero_section',
     )));
 
@@ -109,7 +109,7 @@ function theme_30w_customize_register($wp_customize) {
         'sanitize_callback' => 'esc_url_raw',
     ));
     $wp_customize->add_control(new WP_Customize_Image_Control($wp_customize, 'page404_background', array(
-        'label'   => __('Image en background', 'theme_30w'),
+        'label'   => __('Image en background', 'theme_31w'),
         'section' => 'error-404 not-found',
     )));
 
