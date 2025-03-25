7e64f0ac8d460ca838fe14b7d42aabfe1106a34e
Weiqiang <weiqiangchen19@gmail.com>
2025-03-25 10:37:56 -0400

    Enregistrement des emplacements de menu

--- functions.php
+++ functions.php
@@ -1,4 +1,13 @@
 <?php
+// Inclusion des fichiers de configuration du thème
 include "functions/customizer.php";
 include "functions/options.php";
+
+// Enregistrement des emplacements de menu
+function votre_theme_register_menus() {
+    register_nav_menus( array(
+        'menu_404' => __( 'Menu 404', 'votre-theme' ),
+    ) );
+}
+add_action( 'after_setup_theme', 'votre_theme_register_menus' );
 ?>
