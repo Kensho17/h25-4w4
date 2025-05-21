8d2152914c3841c30396a6f6a0ee0bfa8b0add45
Weiqiang <weiqiangchen19@gmail.com>
2025-05-20 23:41:09 -0400

    avancement des fichiers sur functions et gabarit

--- gabarit/formulaire.php
+++ gabarit/formulaire.php
@@ -3,26 +3,54 @@
         <form action="votre_script_de_traitement.php" method="POST">
             <!-- Champ Nom -->
             <div class="form__reponse">
-                <label for="nom">Nom:</label><br>
-                <input type="text" class="formulaire__input" name="nom" id="nom" placeholder="Écrivez votre nom" required><br>
+                <label for="nom">Nom :</label><br>
+                <input
+                    type="text"
+                    class="formulaire__input"
+                    name="nom"
+                    id="nom"
+                    placeholder="Écrivez votre nom"
+                    required
+                ><br>
             </div>
 
             <!-- Champ Prénom -->
             <div class="form__reponse">
-                <label for="prenom">Prénom:</label><br>
-                <input type="text" class="formulaire__input" name="prenom" id="prenom" placeholder="Écrivez votre prénom" required><br>
+                <label for="prenom">Prénom :</label><br>
+                <input
+                    type="text"
+                    class="formulaire__input"
+                    name="prenom"
+                    id="prenom"
+                    placeholder="Écrivez votre prénom"
+                    required
+                ><br>
             </div>
 
             <!-- Champ Courriel -->
             <div class="form__reponse">
-                <label for="courriel">Courriel:</label><br>
-                <input type="email" class="formulaire__input" name="courriel" id="courriel" placeholder="Écrivez votre courriel" required><br>
+                <label for="courriel">Courriel :</label><br>
+                <input
+                    type="email"
+                    class="formulaire__input"
+                    name="courriel"
+                    id="courriel"
+                    placeholder="Écrivez votre courriel"
+                    required
+                ><br>
             </div>
 
             <!-- Champ Téléphone -->
             <div class="form__reponse">
-                <label for="telephone">Téléphone:</label><br>
-                <input type="tel" class="formulaire__input" name="telephone" id="telephone" placeholder="Écrivez votre téléphone" required><br>
+                <label for="telephone">Téléphone :</label><br>
+                <input
+                    type="tel"
+                    class="formulaire__input"
+                    name="telephone"
+                    id="telephone"
+                    placeholder="Écrivez votre téléphone"
+                    required
+                ><br>
             </div>
 
             <!-- Bouton d'envoi -->
--- gabarit/hero.php
+++ gabarit/hero.php
@@ -1,66 +1,94 @@
 <?php
 // Récupération des options du customizer
-$hero_auteur      = get_theme_mod('hero_auteur', 'Default Title');
-$hero_courriel    = get_theme_mod('hero_courriel', '');
-$couleur_texte    = get_theme_mod('hero_texte', '#ffffff');
-$couleur_icon     = substr(get_theme_mod('hero_icone', '#ffffff'), 1);
+$hero_auteur   = get_theme_mod('hero_auteur', 'Default Title');
+$hero_courriel = get_theme_mod('hero_courriel', '');
+$couleur_texte = get_theme_mod('hero_texte', '#ffffff');
+$couleur_icon  = substr( get_theme_mod('hero_icone', '#ffffff'), 1 );
 
 // On charge les 3 images depuis le customizer
 $hero_background = [];
-for ($i = 0; $i < 3; $i++) {
-    $hero_background[$i] = get_theme_mod("hero_background_{$i}", '');
+for ( $i = 0; $i < 3; $i++ ) {
+    $hero_background[ $i ] = get_theme_mod( "hero_background_{$i}", '' );
 }
 ?>
 <style>
 .hero {
-  color: <?php echo esc_attr($couleur_texte); ?>;
+    color: <?php echo esc_attr( $couleur_texte ); ?>;
 }
 </style>
 
 <section class="hero">
-  <!-- Slides dynamiques -->
-  <?php foreach ($hero_background as $idx => $url) : 
-    if (!$url) continue; // on skip si pas d'image
-  ?>
-    <div
-      class="hero__carrousel<?php echo $idx === 0 ? ' hero__carrousel--active' : ''; ?>"
-      style="background-image: url('<?php echo esc_url($url); ?>');"
-    ></div>
-  <?php endforeach; ?>
-
-  <!-- Radios générées aussi dynamiquement -->
-  <div class="hero__radio">
-    <?php foreach ($hero_background as $idx => $url) : 
-      if (!$url) continue;
+    <!-- Slides dynamiques -->
+    <?php foreach ( $hero_background as $idx => $url ) :
+        if ( ! $url ) {
+            continue; // on skip si pas d'image
+        }
     ?>
-      <input
-        class="hero__radio__input"
-        type="radio"
-        name="hero-carrousel"
-        data-index="<?php echo esc_attr($idx); ?>"
-        <?php checked($idx, 0); ?>
-      >
+        <div
+            class="hero__carrousel<?php echo $idx === 0 ? ' hero__carrousel--active' : ''; ?>"
+            style="background-image: url('<?php echo esc_url( $url ); ?>');"
+        ></div>
     <?php endforeach; ?>
-  </div>
 
-  <!-- Contenu overlay -->
-  <div class="hero__contenu global">
-    <h1 class="hero__titre"><?php bloginfo('name'); ?></h1>
-    <p class="hero__description"><?php bloginfo('description'); ?></p>
-    <?php if ($hero_courriel): ?>
-      <p class="hero__courriel">Courriel : <?php echo esc_html($hero_courriel); ?></p>
-    <?php endif; ?>
-    <p class="hero__addresse">3800, rue Sherbrooke, Montréal</p>
-    <p class="hero__numero">514‑254‑7131</p>
-
-    <div class="hero__icone-app">
-      <img src="https://s2.svgbox.net/social.svg?ic=facebook&color=<?php echo esc_attr($couleur_icon); ?>" width="20" height="20" alt="Facebook">
-      <img src="https://s2.svgbox.net/social.svg?ic=linkedin&color=<?php echo esc_attr($couleur_icon); ?>" width="20" height="20" alt="LinkedIn">
-      <img src="https://s2.svgbox.net/social.svg?ic=paypal&color=<?php echo esc_attr($couleur_icon); ?>" width="20" height="20" alt="PayPal">
-      <img src="https://s2.svgbox.net/social.svg?ic=stackoverflow&color=<?php echo esc_attr($couleur_icon); ?>" width="20" height="20" alt="Stack Overflow">
+    <!-- Radios générées aussi dynamiquement -->
+    <div class="hero__radio">
+        <?php foreach ( $hero_background as $idx => $url ) :
+            if ( ! $url ) {
+                continue;
+            }
+        ?>
+            <input
+                class="hero__radio__input"
+                type="radio"
+                name="hero-carrousel"
+                data-index="<?php echo esc_attr( $idx ); ?>"
+                <?php checked( $idx, 0 ); ?>
+            >
+        <?php endforeach; ?>
     </div>
 
-    <button class="hero__bouton">S'inscrire</button>
-    <p class="hero__auteur">Auteur : Weiqiang Chen</p>
-  </div>
+    <!-- Contenu overlay -->
+    <div class="hero__contenu global">
+        <h1 class="hero__titre"><?php bloginfo( 'name' ); ?></h1>
+        <p class="hero__description"><?php bloginfo( 'description' ); ?></p>
+
+        <?php if ( $hero_courriel ) : ?>
+            <p class="hero__courriel">
+                Courriel : <?php echo esc_html( $hero_courriel ); ?>
+            </p>
+        <?php endif; ?>
+
+        <p class="hero__addresse">3800, rue Sherbrooke, Montréal</p>
+        <p class="hero__numero">514-254-7131</p>
+
+        <div class="hero__icone-app">
+            <img
+                src="https://s2.svgbox.net/social.svg?ic=facebook&color=<?php echo esc_attr( $couleur_icon ); ?>"
+                width="20"
+                height="20"
+                alt="Facebook"
+            >
+            <img
+                src="https://s2.svgbox.net/social.svg?ic=linkedin&color=<?php echo esc_attr( $couleur_icon ); ?>"
+                width="20"
+                height="20"
+                alt="LinkedIn"
+            >
+            <img
+                src="https://s2.svgbox.net/social.svg?ic=paypal&color=<?php echo esc_attr( $couleur_icon ); ?>"
+                width="20"
+                height="20"
+                alt="PayPal"
+            >
+            <img
+                src="https://s2.svgbox.net/social.svg?ic=stackoverflow&color=<?php echo esc_attr( $couleur_icon ); ?>"
+                width="20"
+                height="20"
+                alt="Stack Overflow"
+            >
+        </div>
+
+        <button class="hero__bouton">S'inscrire</button>
+        <p class="hero__auteur">Auteur : Weiqiang Chen</p>
+    </div>
 </section>
